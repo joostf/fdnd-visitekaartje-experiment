e74241e37dadbc94da6cb51594414b54a9b414da
Joost Faber <j.faber@hva.nl>
2021-09-04 16:01:30 +0200

    tweaking the robot

--- scripts/script.js
+++ scripts/script.js
@@ -9,16 +9,16 @@ const   tlMain = new TimelineMax({repeat:-1, paused:false, yoyo:true}),
         leftArm = $('#left_x5F_arm_2'),
         rightArm = $('#right_x5F_arm_2');
    
-   tlMain.set(robot, {x:200, y:200})
+   tlMain.set(robot, {x:200, y:50})
        //.pause()
-       .to(robot, 1, {y:-100, ease: Bounce.easeOut, y: -5})
-       .to(head, .15, {scale:1.2, transformOrigin:'50% 50%'})
+       .to(robot, 1, {y:-30, ease: Bounce.easeOut, y: -5})
+       .to(head, .15, {scale:1.5, transformOrigin:'50% 50%'})
        .to(head, .15, {scale:1, transformOrigin:'50% 50%'})
-       .to(body, .15, {scale:1.2, transformOrigin:'50% 50%'})
+       .to(body, .15, {scale:1.5, transformOrigin:'50% 50%'})
        .to(body, .15, {scale:1, transformOrigin:'50% 50%'})
-       .to(wheels, .15, {scale:1.2, transformOrigin:'50% 50%'})
+       .to(wheels, .15, {scale:1.5, transformOrigin:'50% 50%'})
        .to(wheels, .15, {scale:1, transformOrigin:'50% 50%'})
-       .to(robot, 1, {y:100, ease: Bounce.easeOut, y: -5}, "+=.75");
+       .to(robot, 1, {y:30, ease: Bounce.easeOut, y: -5}, "+=.75");
    
    tlArms.to(leftArm, .25, {rotation:10, transformOrigin:'100% 0%'})
          .to(rightArm, .25, {rotation:-100, transformOrigin:'0% 0%'})
