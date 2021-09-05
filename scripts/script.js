c09ddda9000f68436a25e6b8cd900c4c5583d6bb
Joost Faber <j.faber@hva.nl>
2021-09-05 12:37:34 +0200

    enabled focvus on hidden links

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,48 @@
-// helper functions
-
 const   tlMain = new TimelineMax({repeat:-1, paused:false, yoyo:true}),
-        tlArms = new TimelineMax({repeat:-1, paused:false, yoyo:true}),
+        tlArms = new TimelineMax({repeat:-1, paused:false, yoyo:true});
         robot = $('#robot'),
         head = $('#head_1'),
-        body = $('#body_2'),
+        rBody = $('#body_2'),
         wheels = $('#wheels_2'),
         leftArm = $('#left_x5F_arm_2'),
-        rightArm = $('#right_x5F_arm_2');
+        rightArm = $('#right_x5F_arm_2'),
+        body = document.body;
    
-   tlMain.set(robot, {x:200, y:50})
-       //.pause()
-       .to(robot, 1, {y:-30, ease: Bounce.easeOut, y: -5})
-       .to(head, .15, {scale:1.5, transformOrigin:'50% 50%'})
-       .to(head, .15, {scale:1, transformOrigin:'50% 50%'})
-       .to(body, .15, {scale:1.5, transformOrigin:'50% 50%'})
-       .to(body, .15, {scale:1, transformOrigin:'50% 50%'})
-       .to(wheels, .15, {scale:1.5, transformOrigin:'50% 50%'})
-       .to(wheels, .15, {scale:1, transformOrigin:'50% 50%'})
-       .to(robot, 1, {y:30, ease: Bounce.easeOut, y: -5}, "+=.75");
-   
-   tlArms.to(leftArm, .25, {rotation:10, transformOrigin:'100% 0%'})
-         .to(rightArm, .25, {rotation:-100, transformOrigin:'0% 0%'})
-   
-   
+tlMain.set(robot, {x:200, y:50})
+    //.pause()
+    .to(robot, 1, {y:-30, ease: Bounce.easeOut, y: -5})
+    .to(head, .15, {scale:1.5, transformOrigin:'50% 50%'})
+    .to(head, .15, {scale:1, transformOrigin:'50% 50%'})
+    .to(rBody, .15, {scale:1.5, transformOrigin:'50% 50%'})
+    .to(rBody, .15, {scale:1, transformOrigin:'50% 50%'})
+    .to(wheels, .15, {scale:1.5, transformOrigin:'50% 50%'})
+    .to(wheels, .15, {scale:1, transformOrigin:'50% 50%'})
+    .to(robot, 1, {y:30, ease: Bounce.easeOut, y: -5}, "+=.75");
+
+tlArms.to(leftArm, .25, {rotation:10, transformOrigin:'100% 0%'})
+        .to(rightArm, .25, {rotation:-100, transformOrigin:'0% 0%'})
+  
+/* events */
+
+
+function checkTabPress(e) {
+    if (e.keyCode == 9) {
+        body.classList.add('tabbed')
+    }
+}
+function checkMouseMove() {
+    console.dir(document.activeElement.tagName)
+    if(document.activeElement.tagName !== 'A') {
+        body.classList.remove('tabbed')
+    }
+    //!document.isActiveElement && body.classList.remove('tabbed')  
+}
+
+
+body.addEventListener('keyup', checkTabPress)
+document.addEventListener('mousemove', checkMouseMove)
+
+/* helpers */
 function $ (el) {
     return document.querySelector(el);
 }
